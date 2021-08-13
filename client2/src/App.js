d2701d2266aecdf6a12522bcf02914afb164ba03
Watcharin <twatcharin90@gmail.com>
2021-08-13 16:40:26 +0700

    update class app

--- client2/src/App.js
+++ client2/src/App.js
@@ -1,19 +1,23 @@
-import React from 'react'
-import Footer from './components/Footer';
-import Header from './components/Header';
-import ProductItem from './components/product/ProductItem';
+import React, { Component } from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import ProductItem from "./components/product/ProductItem";
 
-
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <ProductItem productName='IPhone X' unitPrice='40500' />
-      <ProductItem productName='iPad Pro 10.5' unitPrice='30500' />
-      <ProductItem productName='iPad Mini 4 ' unitPrice='19500' />
-      <Footer company="Olanlab" email="olan@olanlab.com" />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <ProductItem productName="IPhone X" unitPrice="40500" />
+        <ProductItem productName="iPad Pro 10.5" unitPrice="30500" />
+        <ProductItem productName="iPad Mini 4 " unitPrice="19500" />
+        <Footer company="Olanlab" email="olan@olanlab.com" />
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+

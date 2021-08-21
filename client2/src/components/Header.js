e94572fcff80ca6b9d5f94e73ad0c3617ed2c0f7
Watcharin <twatcharin90@gmail.com>
2021-08-22 00:26:53 +0700

    50.msg

--- client2/src/components/Header.js
+++ client2/src/components/Header.js
@@ -42,6 +42,7 @@ class Header extends Component {
             </h5>
           </div>
         </div>
+        <hr />
       </div>
     );
   }
--- client2/src/components/monitor/Monitor.js
+++ client2/src/components/monitor/Monitor.js
@@ -6,7 +6,7 @@ import axios from "axios";
 export class Monitor extends Component {
   constructor(props) {
     super(props);
-    this.state = { totalPrice: 0, orders: [] };
+    this.state = { totalPrice: 0, orders: [], confirm: false, msg: "" };
     this.addOrder = this.addOrder.bind(this);
     this.delOrder = this.delOrder.bind(this);
     this.confirmOrder = this.confirmOrder.bind(this);
@@ -23,7 +23,11 @@ export class Monitor extends Component {
       this.state.orders.push({ product: product, quantity: 1 });
     }
     const totalPrice = this.state.totalPrice + parseInt(product.unitPrice);
-    this.setState({ totalPrice: totalPrice, orders: this.state.orders });
+    this.setState({
+      totalPrice: totalPrice,
+      orders: this.state.orders,
+      confirm: false,
+    });
     // console.log(this.state.orders);
   }
 
@@ -37,33 +41,60 @@ export class Monitor extends Component {
     const totalPrice =
       this.state.totalPrice -
       findOrder.quantity * parseInt(findOrder.product.unitPrice);
-    this.setState({ totalPrice: totalPrice, orders: resultOrder });
+    this.setState({
+      totalPrice: totalPrice,
+      orders: resultOrder,
+      confirm: false,
+    });
 
     console.log(resultOrder);
   }
-  
+
   confirmOrder() {
-    const {totalPrice, orders} = this.state;
-
-
-
-    axios.post('http://localhost:3001/orders', {orderedDate: new Date(), totalPrice, orders })
-    .then(res => {
-      this.setState({ totalPrice: 0, orders: [] });
-    })
-    
+    const { totalPrice, orders } = this.state;
+    if (orders && orders.length > 0) {
+      axios
+        .post("http://localhost:3001/orders", {
+          orderedDate: new Date(),
+          totalPrice,
+          orders,
+        })
+        .then((res) => {
+          this.setState({
+            totalPrice: 0,
+            orders: [],
+            confirm: true,
+            msg: "บันทึกรายการสั่งซื้อเรียบร้อย",
+          });
+        });
+    } else {
+      this.setState({
+        totalPrice: 0,
+        orders: [],
+        confirm: true,
+        msg: "เลือกสินค้าก่อนค่ะ",
+      });
+    }
   }
 
   cancelOrder() {
     // console.log("cancelOrder");
-    this.setState({ totalPrice: 0, orders: [] });
+    this.setState({
+      totalPrice: 0,
+      orders: [],
+      confirm: false,
+    });
   }
-
-
 
   render() {
     return (
       <div className="container-fluid">
+        {this.state.confirm && (
+          <div className="alert alert-secondary title text-right" role="alert">
+            {this.state.msg}
+          </div>
+        )}
+
         <div className="row">
           <div className="col-md-9">
             <ProductList

26cbb299fc7660e16099b84ab3f9719fb4171ac7
Watcharin <twatcharin90@gmail.com>
2021-08-26 16:30:36 +0700

    58.404NotFound

--- client2/src/App.js
+++ client2/src/App.js
@@ -5,6 +5,7 @@ import Home from "./containers/Home"
 import About from "./containers/About"
 import Order from "./containers/order/Order"
 import Product from "./containers/product/Product";
+import NotFound from "./containers/error/NotFound";
 
 
 
@@ -18,6 +19,8 @@ class App extends Component {
         <Route exact path="/about" component={About} />
         <Route exact path="/orders" component={Order} />
         <Route exact path="/products" component={Product} />
+
+        <Route component={NotFound} />
       </Switch>
     )
   }

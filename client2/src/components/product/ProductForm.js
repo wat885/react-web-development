d1d48c734f3fb9b42015846511984e800cf5187a
Watcharin <twatcharin90@gmail.com>
2021-09-08 14:11:54 +0700

    76. Field Component #1

--- client2/src/components/product/ProductForm.js
+++ client2/src/components/product/ProductForm.js
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
-import { reduxForm } from "redux-form";
+import { reduxForm, Field } from "redux-form";
 
 export class ProductForm extends Component {
+  renderFields() {
+    const formFields = [
+      { name: "productName", type: "text" },
+      { name: "unitPrice", type: "number" },
+      { name: "thumbnail", type: "text" },
+    ];
+    return formFields.map(({ name, type }) => {
+      return <Field name={name} type={type} component="input" />;
+    });
+  }
+  
+
   render() {
     return (
       <div>
-        <from>
-
-        </from>
+        <from>{this.renderFields()}</from>
       </div>
     );
   }
